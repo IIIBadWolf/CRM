e9161f5ffec81b746ba11f30d94f6cf17dd72297
IIIBadWolf <tkmslot@yandex.ru>
2025-12-22 21:28:35 +0300

    Revert "Первая загрузка"
    This reverts commit bdf7e1b10cb952ac082f4c43cb344f6085a92cbd.

--- logic_price.py
+++
@@ -1,8 +0,0 @@
-# logic_price.py
-from db import add_price_history, get_price_history_for_product
-
-def record_price_if_changed(product_id, new_price):
-    history = get_price_history_for_product(product_id)
-    last_price = history[0]['price'] if history else None
-    if last_price is None or float(new_price) != float(last_price):
-        add_price_history(product_id, new_price)
